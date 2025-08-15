0329ecb4adbaa8d606f9b0b2e0972ce815e877cf
Administrator <chagai33@gmail..com>
2025-08-15 09:23:31 +0300

    add text on loading

--- src/components/Common/LoadingSpinner.tsx
+++ src/components/Common/LoadingSpinner.tsx
@@ -3,8 +3,9 @@
 import React from 'react';
 
 const LoadingSpinner: React.FC = () => (
-  <div className="flex items-center justify-center h-screen bg-gray-50">
+  <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
     <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-orange-500"></div>
+    <p className="mt-4 text-lg text-gray-600">טוען את האפליקציה...</p>
   </div>
 );
 
